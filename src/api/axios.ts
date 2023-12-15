f7d2f35f8b000c18acd272009ec9a0ecd1cc23fc
Ignacio Guzmán <ignacio658mg@gmail.com>
2023-12-15 14:28:39 -0300

    feat!: use process .env files

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import Constants from 'expo-constants';
 
 const API_BASE_URL =
-    Constants.expoConfig?.extra?.API_BASE_URL ?? 'https://ransys-test.vercel.app';
+    process.env.EXPO_PUBLIC_API_BASE_URL ?? 'https://ransys-test.vercel.app';
 
 export const appAxios = axios.create({
     baseURL: `${API_BASE_URL}/api`,
