3363701beba56ff6ab4896866f9cbf98ce7ff22c
Dledes <davidpewen_101@hotmail.com>
2024-10-09 18:50:50 -0300

    fixed visual bug in the add image component, fixed statusbar globally

--- src/screens/Task/AddImage.tsx
+++ src/screens/Task/AddImage.tsx
@@ -10,28 +10,23 @@ type Props = {
 const AddImage = ({ navigateToCameraScreen, selectImage, maxImageAmount }: Props) => {
     const flexPercent = Math.trunc(100 / maxImageAmount);
     return (
-        <View className={`flex-[0.${String(flexPercent)}] relative border border-border`}>
-            <View
-                className="bg-muted flex items-center justify-around"
-                style={{
-                    borderRadius: 6,
-                    aspectRatio: 9 / 16,
-                }}
-            >
-                <TouchableOpacity
-                    onPress={navigateToCameraScreen}
-                    className="items-center"
-                >
-                    <EvilIcons name="camera" size={32} color="#4B5563" />
+        <View
+            className={`flex-[0.${String(flexPercent)}] bg-muted items-center justify-around border border-border`}
+            style={{
+                borderRadius: 6,
+                aspectRatio: 9 / 16,
+            }}
+        >
+            <TouchableOpacity onPress={navigateToCameraScreen} className="items-center">
+                <EvilIcons name="camera" size={32} color="#4B5563" />
 
-                    <Text className="text-[#4B5563] text-xs">Tomar foto</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={selectImage} className="items-center">
-                    <EvilIcons name="image" size={32} color="#4B5563" />
+                <Text className="text-[#4B5563] text-xs">Tomar foto</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={selectImage} className="items-center">
+                <EvilIcons name="image" size={32} color="#4B5563" />
 
-                    <Text className="text-[#4B5563] text-xs">Elegir foto</Text>
-                </TouchableOpacity>
-            </View>
+                <Text className="text-[#4B5563] text-xs">Elegir foto</Text>
+            </TouchableOpacity>
         </View>
     );
 };
