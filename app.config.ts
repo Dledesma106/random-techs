2224f7189b544d657c7b2d1dbaa9eda240c4ad89
David Ledesma <dledesma2020@gmail.com>
2025-03-02 21:50:41 -0300

    fixed owner name

--- app.config.ts
+++ app.config.ts
@@ -48,7 +48,7 @@ const config = ({ config }: ConfigContext): ExpoConfig => ({
         tsconfigPaths: true,
     },
     plugins: ['expo-font', 'expo-secure-store', 'expo-build-properties'],
-    owner: 'dledesma2020',
+    owner: 'undefineddevs',
 });
 
 export default config;
