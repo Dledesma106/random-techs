c7bcfde8b2d15a60f11ab86a6ceced210ef397d2
David Ledesma <dledesma2020@gmail.com>
2025-03-05 15:22:08 -0300

    reverted to a single file app.config file, that handles the project id and name by the project type variable

--- app.config.ts
+++ app.config.ts
@@ -1,19 +1,68 @@
 import 'dotenv/config';
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
-import devConfig from './app.config.dev';
-import prodConfig from './app.config.prod';
+const getProjectId = (type: string | undefined) => {
+    return type === 'development'
+        ? '3d82b726-b563-4a79-aea4-fc3d07cb84b6' // Dev Project ID
+        : '27b396d4-0e33-4573-ab58-f18a85482277'; // Prod Project ID
+};
 
-const config = (context: ConfigContext): ExpoConfig => {
-    console.log('🔧 Current EXPO_PROJECT_TYPE:', process.env.EXPO_PROJECT_TYPE);
+const getAppName = (type: string | undefined) => {
+    return type === 'development' ? 'Tecnicos Random Dev' : 'Tecnicos Random';
+};
 
-    if (process.env.EXPO_PROJECT_TYPE === 'development') {
-        console.log('✅ Using development configuration');
-        return devConfig(context);
-    } else {
-        console.log('✅ Using production configuration');
-        return prodConfig(context);
-    }
+const config = ({ config }: ConfigContext): ExpoConfig => {
+    const projectType = process.env.EXPO_PROJECT_TYPE;
+    console.log('🔧 Current EXPO_PROJECT_TYPE:', projectType);
+
+    return {
+        ...config,
+        name: getAppName(projectType),
+        slug: 'random-techs',
+        version: '0.1.7',
+        orientation: 'portrait',
+        icon: './assets/images/icon.png',
+        scheme: 'myapp',
+        userInterfaceStyle: 'automatic',
+        splash: {
+            image: './assets/images/splash.png',
+            resizeMode: 'contain',
+            backgroundColor: '#ffffff',
+        },
+        updates: {
+            fallbackToCacheTimeout: 0,
+        },
+        assetBundlePatterns: ['**/*'],
+        ios: {
+            supportsTablet: true,
+        },
+        android: {
+            adaptiveIcon: {
+                foregroundImage: './assets/images/adaptive-icon.png',
+                backgroundColor: '#ffffff',
+            },
+            package: 'com.dledesma2020.randomtechs',
+        },
+        web: {
+            favicon: './assets/images/favicon.png',
+        },
+        extra: {
+            eas: {
+                projectId: getProjectId(projectType),
+            },
+            awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            awsRegion: process.env.AWS_REGION,
+            awsBucketName: process.env.AWS_BUCKET_NAME,
+            apiHost: process.env.API_HOST,
+            apiBaseUrl: process.env.API_BASE_URL,
+        },
+        experiments: {
+            tsconfigPaths: true,
+        },
+        plugins: ['expo-font', 'expo-secure-store', 'expo-build-properties'],
+        owner: 'undefineddevs',
+    };
 };
 
 export default config;
