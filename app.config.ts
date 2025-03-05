19dafa6aacb4fd1b49b781697be0ab50b9a48d84
David Ledesma <dledesma2020@gmail.com>
2025-03-05 10:24:37 -0300

    reverted to single version for variables

--- app.config.ts
+++ app.config.ts
@@ -36,26 +36,12 @@ const config = ({ config }: ConfigContext): ExpoConfig => ({
         eas: {
             projectId: '27b396d4-0e33-4573-ab58-f18a85482277',
         },
-        awsAccessKeyId:
-            process.env.ENVIRONMENT === 'production'
-                ? process.env.AWS_ACCESS_KEY_ID
-                : process.env.DEV_AWS_ACCESS_KEY_ID,
-        awsSecretAccessKey:
-            process.env.ENVIRONMENT === 'production'
-                ? process.env.AWS_SECRET_ACCESS_KEY
-                : process.env.DEV_AWS_SECRET_ACCESS_KEY,
-        awsRegion:
-            process.env.ENVIRONMENT === 'production'
-                ? process.env.AWS_REGION
-                : process.env.DEV_AWS_REGION,
-        awsBucketName:
-            process.env.ENVIRONMENT === 'production'
-                ? process.env.AWS_BUCKET_NAME
-                : process.env.DEV_AWS_BUCKET_NAME,
-        apiHost:
-            process.env.ENVIRONMENT === 'production'
-                ? process.env.API_HOST
-                : process.env.DEV_API_HOST,
+        awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        awsRegion: process.env.AWS_REGION,
+        awsBucketName: process.env.AWS_BUCKET_NAME,
+        apiHost: process.env.API_HOST,
+        apiBaseUrl: process.env.API_BASE_URL,
     },
     experiments: {
         tsconfigPaths: true,
